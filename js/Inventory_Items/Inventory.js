e03de87f05dd8dbb07f30ed5291af9b0691cf4c1
Jaime Rivas <jaimerivas81@outlook.com>
2018-12-26 22:54:10 -0800

    adding initial logic to render inventory; hooked up inventory toggle function to modify inventory visibility

--- js/Inventory_Items/Inventory.js
+++ js/Inventory_Items/Inventory.js
@@ -1,8 +1,11 @@
+var isInventoryVisible = false;
+
 Inventory = function()
 {
+
 	var self = {
 		items:[] // {id: "itemId", amount: 1}
-	}
+	};
 
 	self.addItem = function(itemId,amount)
 	{
@@ -12,12 +15,14 @@ Inventory = function()
 			if(self.items[i].itemId == itemId)
 			{
 				self.items[i].amount += amount;
+				self.renderInventory();
 				return;
 			}
 		}
 
 		self.items.push({id: itemId, amount: amount});
-	}
+		self.renderInventory();
+	};
 
 	self.removeItem = function(itemId,amount)
 	{
@@ -30,10 +35,11 @@ Inventory = function()
 				{
 					self.items.splice(i,1);
 				}
+				self.renderInventory();
 				return;
 			}
 		}
-	}
+	};
 
 	self.hasItem = function(itemId,amount)
 	{
@@ -46,16 +52,39 @@ Inventory = function()
 		}
 
 		return false;
-	}
+	};
 
 	self.renderInventory = function()
 	{
-		
-	}
+		var str = "";
+		var image = null;
+
+		for(var i = self.items.length - 1; i >= 0; i--)
+		{
+			let item = Item.List[self.items[i].id];
+			str += item.name;
+			if(item.image != null || item.image != undefined)
+				image = item.image;
+		}
+
+		if(isInventoryVisible)
+		{
+			//draw inventory and display the item's name and image
+			console.log("drawing inventory");
+		}
+		else
+		{
+			//don't draw inventory
+			console.log("not drawing inventory");
+		}
+	};
 
 	return self;
 }
 
 function toggleInventoryAndDrawImage(){
-	console.log("future functionality to be built");
+	//hide or show inventory UI when "I" is pressed
+	isInventoryVisible = !isInventoryVisible;
+
+	console.log("Inventory is visible? " + isInventoryVisible);
 }
