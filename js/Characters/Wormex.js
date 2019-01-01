6e7a6992a42d6d07dce0fff7e185902ea5b68ab1
rybar <ryan.malm@gmail.com>
2018-12-31 23:57:58 -0600

    animation manager works

--- js/Characters/Wormex.js
+++ js/Characters/Wormex.js
@@ -7,7 +7,49 @@ function wormexClass()
 
 	this.exp = new xpClass();//only for init level within a bracket appropriate to enemy
 	this.stats = new statsClass();
-	
+	this.frame = {x: 0, y: 0}
+	this.anims = new AnimManager(this);
+	const { anims } = this;
+	anims.add('walk-up', [
+		{x:0, y: 0},
+		{x:1, y: 0},
+		{x:2, y: 0},
+		{x:3, y: 0},
+		{x:4, y: 0},
+		{x:5, y: 0},
+		{x:6, y: 0},
+		{x:7, y: 0},
+		{x:8, y: 0},
+		{x:9, y: 0},
+		{x:10, y: 0},
+	], 0.1);
+	anims.add('walk-down', [
+		{x:0, y: 1},
+		{x:1, y: 1},
+		{x:2, y: 1},
+		{x:3, y: 1},
+		{x:4, y: 1},
+		{x:5, y: 1},
+		{x:6, y: 1},
+		{x:7, y: 1},
+		{x:8, y: 1},
+		{x:9, y: 1},
+		{x:10, y: 1},
+	], 0.1);
+	anims.add('walk-right', [
+		{x:0, y: 2},
+		{x:1, y: 2},
+		{x:2, y: 2},
+		{x:3, y: 2},
+		{x:4, y: 2},
+		{x:5, y: 2},
+		{x:6, y: 2},
+		{x:7, y: 2},
+		{x:8, y: 2},
+		{x:9, y: 2},
+		{x:10, y: 2},
+	], 0.1);
+	anims.play('walk-down');
 	this.SpritePic = wormexPic;
 	this.x = 150;
 	this.y = 150;
@@ -41,7 +83,8 @@ function wormexClass()
 			nextY += this.velY;
 
 			this.superClassMove(nextX,nextY);
-		} 
+		}
+		this.anims.update(1/fps); 
 	}
 	
 	
@@ -49,25 +92,33 @@ function wormexClass()
 	
 	this.draw = function()
 	{
-		this.tickCount++;
-		
-		if (this.tickCount > this.ticksPerFrame) // advance the frame
-		{
-			
-			this.tickCount = 0;
-			
-			if(this.frameIndex < this.numberOfFrames-1) // frame moves to the next number
-			{
-				this.frameIndex += 1;
-			} else  // frame moves from the last frame to the first frame 
-				{ 
-				this.frameIndex = 0;
-			}
+		if(this.velY > 0){
+			anims.play('walk-down');
 		}
+		else anims.play('walk-up');
+		// this.tickCount++;
 		
-		this.sx = this.frameIndex * this.width;		// This is the Frame the Sprite is on	
+		// if (this.tickCount > this.ticksPerFrame) // advance the frame
+		// {
+			
+		// 	this.tickCount = 0;
+			
+		// 	if(this.frameIndex < this.numberOfFrames-1) // frame moves to the next number
+		// 	{
+		// 		this.frameIndex += 1;
+		// 	} else  // frame moves from the last frame to the first frame 
+		// 		{ 
+		// 		this.frameIndex = 0;
+		// 	}
+		// }
+		
+		// this.sx = this.frameIndex * this.width;		// This is the Frame the Sprite is on	
 
-		debugDrawHeading(this)
-		drawSprite(this.SpritePic, this.sx, this.sy, this.width, this.height, this.centerX, this.centerY);
+		// debugDrawHeading(this)
+		
+
+		let frameX = anims.frameSource.x * this.width;
+		let frameY = anims.frameSource.y * this.height; 
+		drawSprite(this.SpritePic, frameX, frameY, this.width, this.height, this.centerX, this.centerY);
 	}	
 }
