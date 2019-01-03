c713f4b05119763b5c3563a8aa5f441fa6403cbb
Terrence <t.w.mcdonnell10@gmail.com>
2019-01-03 14:18:34 -0700

    Projectile class now takes image, rotation

--- js/Characters/Projectile.js
+++ js/Characters/Projectile.js
@@ -1,27 +1,33 @@
-function projectileClass(startX,startY,vX,vY)
+function projectileClass(startX,startY,vX,vY,time, rotation, image = undefined)
 {
 	this.x = startX;
 	this.y = startY;
 	this.velX = vX;
 	this.velY = vY;
-	this.life = 50;
+	this.life = time;
+	this.image = image;
+	this.rotation = rotation;
 
 	this.reset = function()
 	{
-		console.log("resetting projectile");
+		console.log("projectile finished");
 	}
 
 	this.move = function()
 	{
 		this.life--;
 
-		this.x += this.velX;
-		this.y += this.velY;
+		this.x -= Math.cos(this.rotation) * this.velX;
+		this.y -= Math.sin(this.rotation) * this.velY;
 	}
 
 	this.draw = function()
 	{
-		drawCircle(this.x,this.y, 5, "red");
+		if (this.image != undefined) {
+			drawBitmapCenteredWithRot(this.image, this.x, this.y, this.rotation);
+		} else {
+			drawCircle(this.x,this.y, 5, "red"); // no projectile image - using default
+		}
 	}
 
 	this.isReadyToRemove = function()
