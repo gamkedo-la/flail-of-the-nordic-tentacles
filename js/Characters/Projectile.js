bfd17ba73fb1abc33cd333f4da9e1f8c842a81e8
Terrence <t.w.mcdonnell10@gmail.com>
2019-01-03 15:09:07 -0700

    Projectile class checks for image and inits appropirately. Projectiles can now collide with player, cause damage and checks if player.stats.hp <= 0.

--- js/Characters/Projectile.js
+++ js/Characters/Projectile.js
@@ -5,7 +5,25 @@ function projectileClass(startX,startY,vX,vY,time, rotation, image = undefined)
 	this.velX = vX;
 	this.velY = vY;
 	this.life = time;
+	this.damage = 50;
+	this.radius = 5;
+	var noImage = true;
 	this.image = image;
+	if (this.image != undefined) 
+	{
+		noImage = false;
+	}
+	this.width = (!noImage) ? this.image.width : this.radius * 2;
+	this.height = (!noImage) ? this.image.height : this.radius * 2;
+	if (noImage) 
+	{
+		this.collider = new colliderClass(this.x,this.y,this.width - 2,this.height - 2,0,0);
+	}
+	else 
+	{
+		this.collider = new colliderClass(this.x,this.y,this.width - 10,this.height - 16,0,0);
+	}
+	this.collided = false;
 	this.rotation = rotation;
 
 	this.reset = function()
@@ -19,14 +37,34 @@ function projectileClass(startX,startY,vX,vY,time, rotation, image = undefined)
 
 		this.x -= Math.cos(this.rotation) * this.velX;
 		this.y -= Math.sin(this.rotation) * this.velY;
+		this.collider.update(this.x,this.y);
+		if (this.collider.isCollidingWithOtherCollider(player.collider) &&
+			!this.collided) 
+		{
+			this.collided = true;
+			player.stats.hp -= this.damage;
+			if (player.stats.hp <= 0)
+			{
+				player.stats.isCharacterDead = true;
+			}
+			this.life = 0;
+		}
 	}
 
 	this.draw = function()
 	{
-		if (this.image != undefined) {
+		if (this.image != undefined) 
+		{
 			drawBitmapCenteredWithRot(this.image, this.x, this.y, this.rotation);
-		} else {
-			drawCircle(this.x,this.y, 5, "red"); // no projectile image - using default
+		} 
+		else 
+		{
+			drawCircle(this.x,this.y, this.radius, "blue"); // no projectile image - using default
+		}
+
+		if (debugState) 
+		{
+			this.collider.draw();
 		}
 	}
 
