44f101ba69396cba634eb7c977cab93156c5de25
Jaime Rivas <jaimerivas81@outlook.com>
2019-01-04 21:36:38 -0800

    Still working on better implementation of animation code to avoid DRY and just have the Enemy.js handle it alone

--- js/Utilities/SpriteUtilities.js
+++ js/Utilities/SpriteUtilities.js
@@ -1,3 +1,16 @@
+function animatedSprite(image,sw,sh)
+{
+	this.image = image;
+	this.width = sw;
+	this.height = sh;
+
+	this.draw = function(sx,sy,x,y)
+	{
+		canvasContext.drawImage(this.image, sx, sy, this.width, this.height, 
+							x-this.width*0.5, y-this.height*0.5, this.width, this.height);
+	}
+}
+
 function drawSprite(SpritePic, spriteSheetSourceX, spriteSheetSourceY, spriteSheetFrameWidth, spriteSheetFrameHeight, centerX, centerY) 
 {
 	canvasContext.drawImage(SpritePic, spriteSheetSourceX, spriteSheetSourceY, spriteSheetFrameWidth, spriteSheetFrameHeight, 
