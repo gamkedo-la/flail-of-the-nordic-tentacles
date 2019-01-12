f0e070acb97e2b03ab8d473d65d622608c3f5847
McFunkypants <breakdance@mcfunkypants.com>
2019-01-11 20:12:10 -0800

    stats GUI now uses a bezel overlay png

--- js/Graphics_Audio/ImageLoading.js
+++ js/Graphics_Audio/ImageLoading.js
@@ -21,6 +21,7 @@ var eyepatchPic = document.createElement("img");
 var beaconPic = document.createElement("img");
 var dictionaryPic = document.createElement("img");
 var tentaclePic = document.createElement("img");
+var statsBezelPic = document.createElement("img");
 
 //Misc.
 var footStepsPic = document.createElement("img");
@@ -47,6 +48,7 @@ function loadImages()
 		{varName: wormholePic, fileName: "items/wormhole_quick.png"},
 		{varName: dictionaryPic, fileName: "items/dig_dictionary_quick.png"},
 		{varName: beaconPic, fileName: "items/beacon.png"},
+		{varName: statsBezelPic, fileName: "ui/statsBezel.png"},
 
 		//Effects
 	    {varName: greenSplat, fileName: "effects/green_splatter.png"},
--- js/Main.js
+++ js/Main.js
@@ -100,6 +100,37 @@ function drawAll()
 	canvasContext.restore();
 }
 
+function drawStatsBox() {
+	// background:
+	// drawRect(24,34, 116,122, "#888");
+	// HP:
+	drawRect(30,40, 104,28, "#ddd");
+	drawText(`HP: ${player.stats.hp}/${player.stats.maxHp}`, 32, 50, 'black', '10px sans-serif');
+	drawRect(32,55, 100,10, "white");
+	drawRect(32,55, Math.ceil(player.stats.hp / player.stats.maxHp * 100),10, "red");
+	// XP:
+	drawRect(30,75, 104,24, "#ddd");
+	drawText(`XP: ${player.exp.currentXp}/${player.exp.nextXp}`, 32, 85, 'black', '10px sans-serif');
+	drawText(`Lvl: ${player.exp.currentLvl} `, 100, 85, 'black', '10px sans-serif');
+	drawRect(32,90, 100,6, "white");
+	drawRect(32,90, Math.ceil(player.exp.nextXp / player.exp.currentXp * 100),6, "purple");
+	// STR:
+	drawRect(30,106, 104,24, "#ddd");
+	drawText(`STR: ${player.stats.str}/${player.stats.maxStr}`, 32, 118, 'black', '10px sans-serif');
+	drawRect(32,120, 100,6, "blue");
+	drawRect(32,120, Math.ceil(player.stats.str / player.stats.maxStr* 100),6, "grey");
+	// DEF:
+	drawRect(30,126, 104,24, "#ddd");
+	drawText(`DEF: ${player.stats.def}/${player.stats.maxDef}`, 32, 138, 'black', '10px sans-serif');
+	drawRect(32,140, 100,6, "blue");
+	drawRect(32,140, Math.ceil(player.stats.def / player.stats.maxDef* 100),6, "grey");
+	//drawText(`MP: ${player.stats.mp}/${player.stats.maxMp}`, 32, 168, 'black', '10px sans-serif');
+
+	canvasContext.drawImage(statsBezelPic,24,34);
+
+
+}
+
 //will go to UI JS once implemented
 function drawUI()
 {
@@ -120,30 +151,7 @@ function drawUI()
 	drawText("Player level: " + player.exp.currentLvl, 20, 540, "Maroon", "20px Arial", "center", 10);
 	drawText("Next level: " + player.exp.nextLvl, 20, 560, "Maroon", "20px Arial", "center", 10);
 
-	// background:
-	drawRect(24,34, 116,130, "#888");
-	// HP:
-	drawRect(30,40, 104,28, "#ddd");
-	drawText(`HP: ${player.stats.hp}/${player.stats.maxHp}`, 32, 50, 'black', '10px sans-serif');
-	drawRect(32,55, 100,10, "white");
-	drawRect(32,55, Math.ceil(player.stats.hp / player.stats.maxHp * 100),10, "red");
-	// XP:
-	drawRect(30,75, 104,24, "#ddd");
-	drawText(`XP: ${player.exp.currentXp}/${player.exp.nextXp}`, 32, 85, 'black', '10px sans-serif');
-	drawText(`Lvl: ${player.exp.currentLvl} `, 100, 85, 'black', '10px sans-serif');
-	drawRect(32,90, 100,6, "white");
-	drawRect(32,90, Math.ceil(player.exp.nextXp / player.exp.currentXp * 100),6, "purple");
-	// Stats:
-	drawRect(30,106, 104,24, "#ddd");
-	drawText(`STR: ${player.stats.str}/${player.stats.maxStr}`, 32, 118, 'black', '10px sans-serif');
-	drawRect(32,120, 100,6, "blue");
-	drawRect(32,120, Math.ceil(player.stats.str / player.stats.maxStr* 100),6, "grey");
-
-	drawRect(30,126, 104,24, "#ddd");
-	drawText(`DEF: ${player.stats.def}/${player.stats.maxDef}`, 32, 138, 'black', '10px sans-serif');
-	drawRect(32,140, 100,6, "blue");
-	drawRect(32,140, Math.ceil(player.stats.def / player.stats.maxDef* 100),6, "grey");
-	//drawText(`MP: ${player.stats.mp}/${player.stats.maxMp}`, 32, 168, 'black', '10px sans-serif');
+	drawStatsBox();
 
 	if(isInventoryVisible)
 	{
