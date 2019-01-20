1016f9d48c58b191189c1ea70b31fa241d3def93
Vaan <e.t.khani@gmail.com>
2019-01-20 01:20:55 -0800

    Updating menu and background position

--- js/Interface/Menu.js
+++ js/Interface/Menu.js
@@ -11,25 +11,27 @@ const Menu = new (function() {
     const CHAPTER_PAGE = 5;
      const PAUSED_PAGE = 6;
 
-    let itemsX = 340;
-    let topItemY = 210;
-    let itemsWidth = 300;
-    let rowHeight = 45;
+    let itemsX = 310;
+    let topItemY = 390;
+    let itemsWidth = 650;
+    let rowHeight = 35;
 
     let currentPage = 0;
 
     let textFontFace = "26px Book Antiqua";
     let textColour = "teal" ;
 
-    let classListMenu = ["new game", "resume", "settings", "tutorials" , "credits"];
-    let classListLoad = ["load game", "start chapter", "back"];
+    let classListMenu = ["new*game", "load game", "settings", "tutorials" , "credits"];
+    let classListLoad = ["resume", "select chapter", "back"];
     let classListLevels = ["chapter 1", "chapter 2", "chapter 3", "back"];
     let classListSettings = ["volume", "controls", "back"];
-    let classListHelp= ["gameplay guide","gamepad guide","back"];
-    let classListCredits= ['Jaime Rivas' , "back"];
+    let classListHelp= ["gameplay","gamepad","back"];
     let classListPaused= ['save' , 'audio',  'return'];
+    let classListCredits= ['Jaime Rivas' , "back"];
+    
 
     let menuPageText = [classListMenu, classListLoad, classListSettings, classListHelp, classListCredits, classListLevels, classListPaused];
+
 this.menuMouse = function(){
      for (let i = 0; i < menuPageText[currentPage].length; i++) {
         if(mouseX > itemsX && mouseX < itemsX + itemsWidth 
@@ -53,11 +55,11 @@ this.update = function(){
 
 this.checkState = function(){
      switch (menuPageText[currentPage][this.cursor1]) {
-    case "new game":
+    case "new*game":
         gameIsStarted = true;
         this.cursor1 = 0;
         break;
-    case "resume":
+    case "load game":
         currentPage = RESUME_PAGE;
         this.cursor1 = 0;
         break;
@@ -80,10 +82,10 @@ this.checkState = function(){
     case "controls":
         console.log("TODO Added Controls changer");   
         break;
-    case "game guide":
+    case "gameplay":
         console.log("TODO implement how to play");
         break;
-    case "gamepad layout":
+    case "gamepad":
         console.log("TODO implement control layout");
         break;
     case "back":
@@ -125,6 +127,7 @@ this.draw = function() {
           currentPage = MENU_PAGE;  
         }
         this.redraw();
+        canvasContext.drawImage(logoPic, 50, -150);
     }else {
         currentPage = PAUSED_PAGE;
         canvasContext.clearRect(itemsX -50,topItemY - rowHeight,
@@ -144,17 +147,14 @@ this.draw = function() {
       for (let i = 0; i < classListCredits.length; i++) {
         drawText(classListCredits[i],creditsX, creditsTopY + creditsLineSkipY * i, textColour, textFontFace, 'left', 'top');
       }    
-    } else {
-    if (gameIsStarted === false){
-    canvasContext.drawImage(logoPic, 300, 80);
     }
-          //Draw cursor
-    canvasContext.drawImage(arrowPic,itemsX -wobble ,topItemY + (this.cursor1 * rowHeight) -12);
 
     for (let i=0; i<menuPageText[currentPage].length; i++){
-     drawText(menuPageText[currentPage][i], itemsX,topItemY + rowHeight * i,textColour, textFontFace, 'left', 'top');
+     drawText(menuPageText[currentPage][i],itemsX,topItemY + rowHeight * i,textColour, textFontFace, 'left', 'top');
+    //Draw cursor
+    canvasContext.drawImage(arrowPic,(itemsX-20) - wobble * i,topItemY + (this.cursor1 * rowHeight) -14);
     }   
-  }
+    
 }
    
 })(); 
